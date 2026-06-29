455939e46105ad0998dbb5188da9d1c2c082264e
marinaaa23 <163111357+marinaaa23@users.noreply.github.com>
2026-06-29 08:35:47 +0400

    Update app.py

--- app.py
+++ app.py
@@ -325,4 +325,6 @@ def update_order(order_id):
 if __name__ == '__main__':
     with app.app_context():
         init_db()
-    app.run(debug=True, port=5001)
+    import os
+    port = int(os.environ.get("PORT", 5001))
+    app.run(debug=False, host='0.0.0.0', port=port)
